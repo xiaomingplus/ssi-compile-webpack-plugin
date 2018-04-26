772bded4ac97746b0611f46191169449a727c3a2
guobaoyang <guobaoyang@tencent.com>
2018-04-26 18:18:38 +0800

    for support proxy and path rewrite

---
+++ utils/path-rewrite.js
@@ -0,0 +1,77 @@
+var _ = require('lodash')
+
+module.exports = {
+  create: createPathRewriter
+}
+
+/**
+ * Create rewrite function, to cache parsed rewrite rules.
+ *
+ * @param {Object} rewriteConfig
+ * @return {Function} Function to rewrite paths; This function should accept `path` (request.url) as parameter
+ */
+function createPathRewriter (rewriteConfig) {
+  var rulesCache
+  console.log('rewriteConfig',rewriteConfig);
+  if (!isValidRewriteConfig(rewriteConfig)) {
+    return
+  }
+
+  if (_.isFunction(rewriteConfig)) {
+    var customRewriteFn = rewriteConfig
+    return customRewriteFn
+  } else {
+    rulesCache = parsePathRewriteRules(rewriteConfig)
+    return rewritePath
+  }
+
+  function rewritePath (path) {
+    var result = path
+
+    _.forEach(rulesCache, function (rule) {
+      if (rule.regex.test(path)) {
+        result = result.replace(rule.regex, rule.value)
+        return false
+      }
+    })
+
+    return result
+  }
+}
+
+function isValidRewriteConfig (rewriteConfig) {
+  if (_.isFunction(rewriteConfig)) {
+    return true
+  } else if (!_.isEmpty(rewriteConfig) && _.isPlainObject(rewriteConfig)) {
+    return true
+  } else if (_.isUndefined(rewriteConfig) ||
+               _.isNull(rewriteConfig) ||
+               _.isEqual(rewriteConfig, {})) {
+    return false
+  } else {
+    throw new Error("path rewrite error");
+  }
+}
+
+function parsePathRewriteRules (rewriteConfig) {
+  var rules = []
+
+  if (_.isPlainObject(rewriteConfig)) {
+    _.forIn(rewriteConfig, function (value, key) {
+      rules.push({
+        regex: new RegExp(key),
+        value: rewriteConfig[key]
+      })
+    })
+  }
+
+  return rules
+}
+
+
+// var x=  createPathRewriter({
+//     "^api":"test"
+// });
+// console.log('x',x);
+// var y = x('api');
+// console.log('y',y);
